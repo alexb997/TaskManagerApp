88d7d8879f2239057557e7830eac2600489eb1f4
alexb997 <alex_alexandru_997@yahoo.ro>
2024-09-04 11:49:39 +0300

    notification design

--- front/src/components/NotificationModal.js
+++ front/src/components/NotificationModal.js
@@ -9,26 +9,52 @@ const NotificationModal = ({ show, handleClose, tasksDueToday, newTasks }) => {
       </Modal.Header>
       <Modal.Body>
         <h5>Tasks Due Today</h5>
-        {tasksDueToday.length > 0 ? (
+        {tasksDueToday ? (
           <ListGroup>
             {tasksDueToday.map((task) => (
-              <ListGroup.Item key={task.id}>
-                {task.title} - Due:{" "}
-                {new Date(task.dueDate).toLocaleDateString()}
-                {task.description.substring(0, 20)}
+              <ListGroup.Item
+                key={task.id}
+                className="mb-2 p-3 rounded shadow-sm bg-light"
+                style={{ borderLeft: "5px solid #007bff" }}
+              >
+                <div className="d-flex justify-content-between align-items-center">
+                  <div>
+                    <strong>{task.name}</strong> <br />
+                    <small className="text-muted">
+                      Due: {new Date(task.dueDate).toLocaleDateString()}
+                    </small>
+                  </div>
+                </div>
+                <div className="mt-2 text-muted">
+                  {task.description.substring(0, 30)}...
+                </div>
               </ListGroup.Item>
             ))}
           </ListGroup>
         ) : (
           <p>No tasks due today!</p>
         )}
+
         <h5 className="mt-4">New Tasks Since Last Login</h5>
         {newTasks ? (
           <ListGroup>
             {newTasks.map((task) => (
-              <ListGroup.Item key={task.id}>
-                {task.title} - Created:{" "}
-                {new Date(task.creationDate).toLocaleDateString()}
+              <ListGroup.Item
+                key={task.id}
+                className="mb-2 p-3 rounded shadow-sm bg-light"
+                style={{ borderLeft: "5px solid #28a745" }}
+              >
+                <div className="d-flex justify-content-between align-items-center">
+                  <div>
+                    <strong>{task.name}</strong> <br />
+                    <small className="text-muted">
+                      Due Date: {new Date(task.dueDate).toLocaleDateString()}
+                    </small>
+                  </div>
+                </div>
+                <div className="mt-2 text-muted">
+                  {task.description.substring(0, 30)}...
+                </div>
               </ListGroup.Item>
             ))}
           </ListGroup>
